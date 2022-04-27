7af618f9f3af57340b5d0fa43bd4c14d20c0b023
Cmacle <cmacle4500@gmail.com>
2022-04-27 16:35:32 -0500

    Ai is working somewhat

---
+++ game_deterministic.py
@@ -0,0 +1,237 @@
+import numpy as np
+
+last_num_added = 4
+
+class TwentyFortyEight():
+    score = 0
+    game_over = False
+    
+    def __init__(self, width=4, height=4):
+        self.width = width
+        self.height = height
+        self.board = np.zeros(shape=(height,width), dtype=np.uint16)
+        self.add_num()
+        self.add_num()
+        
+    def __str__(self):
+        string = ""
+        for y in range(self.height):
+            string += "\n\n" + "|"
+            for x in range(self.width):
+                if self.board[y][x]:
+                    string += str(self.board[y][x]).center(8) + "|"
+                else:
+                    string += "        " + "|"
+        return string
+    
+    def add_num(self):
+        # Generate a new number in an empty space
+        # with value 2 or 4
+        empty_spaces = self.empty_spaces
+        if not empty_spaces:
+            self.game_over = True
+            return "GAME OVER"
+        else:
+            loc = np.random.randint(len(empty_spaces))
+            y = empty_spaces[loc][0]
+            x = empty_spaces[loc][1]
+            self.board[y][x] = np.random.randint(low=1,high=3)*2
+            return (y,x)
+    
+    def move(self, direction):
+        # Move in a direction
+        if direction == "Up" or direction == "u":
+            if self.can_move('u'):
+                self._move_up()
+                self.add_num()
+        elif direction == "Down" or direction == "d":
+            if self.can_move('d'):
+                self._move_down()
+                self.add_num()
+        elif direction == "Left" or direction == "l":
+            if self.can_move('l'):
+                self._move_left()
+                self.add_num()
+        elif direction == "Right" or direction == "r":
+            if self.can_move('r'):
+                self._move_right()
+                self.add_num()
+        # Check for game over
+        if not self.available_moves:
+            self.game_over=True
+    
+    def _move_up(self):
+        for y in range(1,self.height):
+            for x in range(self.width):
+                if self.board[y][x]:
+                    holdy = y
+                    at_zero = False
+                    while True:
+
+                        if self.board[holdy-1][x] == 0:
+                            self.board[holdy-1][x] = self.board[holdy][x]
+                            self.board[holdy][x] = 0
+                            holdy-=1
+                        else:
+                            break
+                        if holdy < 1:
+                            at_zero = True
+                            break
+                    if not at_zero:
+                        if self.board[holdy][x] == self.board[holdy-1][x]:
+                            loc1 = (holdy-1,x)
+                            loc2 = (holdy,x)
+                            self._combine(loc1,loc2)
+                else:
+                    continue
+    
+    def _move_down(self):
+        for y in range(self.height-2,-1,-1):
+            for x in range(self.width):
+                if self.board[y][x]:
+                    holdy = y
+                    at_max = False
+                    while True:
+                        if self.board[holdy+1][x] == 0:
+                            self.board[holdy+1][x] = self.board[holdy][x]
+                            self.board[holdy][x] = 0
+                            holdy+=1
+                        else:
+                            break
+                        if holdy >= self.height-1:
+                            at_max = True
+                            break
+                    if not at_max:
+                        if self.board[holdy][x] == self.board[holdy+1][x]:
+                            loc1 = (holdy+1,x)
+                            loc2 = (holdy,x)
+                            self._combine(loc1,loc2)
+                else:
+                    continue
+                
+    def _move_left(self):
+        for x in range(1,self.width):
+            for y in range(self.height):
+                if self.board[y][x]:
+                    holdx = x
+                    at_zero = False
+                    while True:
+                        if self.board[y][holdx-1] == 0:
+                            self.board[y][holdx-1] = self.board[y][holdx]
+                            self.board[y][holdx] = 0
+                            holdx-=1
+                        else:
+                            break
+                        if holdx < 1:
+                            at_zero = True
+                            break
+                    if not at_zero:
+                        if self.board[y][holdx] == self.board[y][holdx-1]:
+                            loc1 = (y,holdx-1)
+                            loc2 = (y,holdx)
+                            self._combine(loc1,loc2)
+                else:
+                    continue
+    
+    def _move_right(self):
+        for x in range(self.width-2,-1,-1):
+            for y in range(self.height):
+                if self.board[y][x]:
+                    holdx = x
+                    at_max = False
+                    while True:
+                        if self.board[y][holdx+1] == 0:
+                            self.board[y][holdx+1] = self.board[y][holdx]
+                            self.board[y][holdx] = 0
+                            holdx+=1
+                        else:
+                            break
+                        if holdx >= self.width-1:
+                            at_max = True
+                            break
+                    if not at_max:
+                        if self.board[y][holdx] == self.board[y][holdx+1]:
+                            loc1 = (y,holdx+1)
+                            loc2 = (y,holdx)
+                            self._combine(loc1,loc2)
+                else:
+                    continue
+    
+    def _combine(self, loc1, loc2):
+        # Combine the two locations, increment the score and
+        # set loc2 to 0
+        self.board[loc1[0]][loc1[1]] *= 2
+        self.board[loc2[0]][loc2[1]] = 0
+        self.score += self.board[loc1[0]][loc1[1]]
+    
+    def can_move(self, direction):
+        # Check if a move will cause any changes
+        if direction == "Up" or direction == "u":
+            for y in range(1,self.height):
+                for x in range(self.width):
+                    if self.board[y][x]:
+                        if ((not self.board[y-1][x]) or 
+                            self.board[y][x] == self.board[y-1][x]):
+                            return True
+                    else:
+                        continue
+            return False
+        elif direction == "Down" or direction == "d":
+            for y in range(self.height-2,-1,-1):
+                for x in range(self.width):
+                    if self.board[y][x]:
+                        if ((not self.board[y+1][x]) or 
+                            self.board[y][x] == self.board[y+1][x]):
+                            return True
+                    else:
+                        continue
+            return False
+        elif direction == "Left" or direction == "l":
+            for x in range(1,self.width):
+                for y in range(self.height):
+                    if self.board[y][x]:
+                        if ((not self.board[y][x-1]) or 
+                            self.board[y][x] == self.board[y][x-1]):
+                            return True
+                    else:
+                        continue
+            return False
+        elif direction == "Right" or direction == "r":
+            for x in range(self.width-2,-1,-1):
+                for y in range(self.height):
+                    if self.board[y][x]:
+                        if ((not self.board[y][x+1]) or 
+                            self.board[y][x] == self.board[y][x+1]):
+                            return True
+                    else:
+                        continue
+            return False
+    
+    @property
+    def empty_spaces(self):
+        # Return a list of empty spaces (value = 0)
+        empty_spaces = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if not self.board[y][x]:
+                    space = (y,x)
+                    empty_spaces.append(space)
+        return empty_spaces
+                     
+    @property
+    def available_moves(self):
+        # Return the number of available moves
+        available_moves = 0
+        for dir in ('u','d','l','r'):
+            if self.can_move(dir):
+                available_moves += 1
+        return available_moves
+
+
+if __name__ == '__main__':
+    game = TwentyFortyEight()
+    while not game.game_over:
+        print(game)
+        print(f'Score: {game.score}')
+        game.move(input())
+    print("GAME OVER")
---
+++ neat_ai.py
@@ -0,0 +1,97 @@
+"""
+2-input XOR example -- this is most likely the simplest possible example.
+"""
+
+from __future__ import print_function
+import math
+import os
+import neat
+import visualize
+import TwentyFortyEight
+
+outputs = ["u", "d", "l", "r"]
+os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin'
+num_games = 5
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        genome.fitness = play_game(net, config, games=num_games)[0]
+
+def play_game(net, config, genome=None, games=1):
+    scores = []
+    moves = []
+    for i in range(games):
+        
+        game = TwentyFortyEight.TwentyFortyEight()
+        num_moves = 0
+        stale_moves = 0
+        board = []
+        last_board = []
+        while not game.game_over and stale_moves <= 10:
+            board = []
+            for y in range(len(game.board)):
+                for x in range(len(game.board[0])):
+                    board.append(int(game.board[y][x]))
+                
+            num_moves += 1
+            output = net.activate(board)
+            outputs_sorted = [(output[i],i) for i in range(len(output))]
+            outputs_sorted.sort()
+            for output in outputs_sorted:
+                if game.can_move(outputs[output[1]]):
+                    game.move(outputs[output[1]])
+                    if genome:
+                        tup = (game.board.copy(), outputs[outputs_sorted[0][1]], outputs[output[1]])
+                        genome.moves.append(tup)
+        scores.append(game.score)
+        moves.append(num_moves)
+    return sum(scores)/len(scores), sum(moves)//len(moves)
+    
+def run(config_file):
+    # Load configuration.
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 300 generations.
+    winner = p.run(eval_genomes, 300)
+
+    # Display the winning genome.
+    print('\nBest genome:\n{!s}'.format(winner))
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    winner.moves=[]
+    high_score, num_moves = play_game(winner_net, config, genome=winner, games=num_games)
+    print('\nMoves:')
+    for move in winner.moves:
+        print(move[0])
+        print(f'First Choice: {move[1]} Taken: {move[2]}')
+    print(f'Score: {high_score} Num Moves: {num_moves}')
+
+    visualize.draw_net(config, winner, True)
+    visualize.plot_stats(stats, ylog=False, view=True)
+    visualize.plot_species(stats, view=True)
+
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    p.run(eval_genomes, 10)
+
+
+if __name__ == '__main__':
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'neat_config')
+    run(config_path)
